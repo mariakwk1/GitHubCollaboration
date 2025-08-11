d52ed870ab0be35527f8c86a7fdb22b50c46e870
Eleanor Abitbol <eleanor.abitbol@gmail.com>
2025-08-11 10:40:41 -0400

    text change eleanor

--- GitHubCollaboration/ContentView.swift
+++ GitHubCollaboration/ContentView.swift
@@ -16,6 +16,7 @@ struct ContentView: View {
             Text("Hello, world!")
             Text ( "Change 1!")
             Text("Change 3!")
+            Text("change 5")
 
         }
         .padding()
